f5f3233c5e64b13eeaf6531bff0baccb8bc5e882
Henrique Souza <hszm2094@outlook.com>
2025-04-13 02:22:18 -0300

    Add Dapr, Redis, and MongoDB support to the project
    Updated `DaprAspire.AppHost.csproj` to include package references for MongoDB, Redis, and the Community Toolkit for Dapr. Added project references for `DaprAspire.ConsolidationApi` and `DaprAspire.Entries.Api`.
    
    Modified `Program.cs` to integrate Redis and MongoDB services, added Dapr support, and configured projects to use Dapr sidecars.

--- DaprAspire.AppHost/Program.cs
+++ DaprAspire.AppHost/Program.cs
@@ -1,3 +1,16 @@
-var builder = DistributedApplication.CreateBuilder(args);
+﻿var builder = DistributedApplication.CreateBuilder(args);
+
+var redis = builder.AddRedis("redis");
+
+var mongodb = builder.AddMongoDB("mongodb");
+
+builder.AddDapr();
+
+builder.AddProject<Projects.DaprAspire_Entries_Api>("dapraspire-entries-api")
+       .WithDaprSidecar()
+       .WithReference(mongodb);
+
+builder.AddProject<Projects.DaprAspire_ConsolidationApi>("dapraspire-consolidationapi")
+       .WithDaprSidecar();
 
 builder.Build().Run();
