ea1a423cb5bc6780e2883f99b964e38861868141
Henrique Souza <hszm2094@outlook.com>
2025-04-13 21:47:21 -0300

    Add infrastructure project and Implement projection repository
    - Updated `DaprAspire.Consolidation.Infrastructure.csproj` to target .NET 9.0, enabling implicit usings and nullable reference types. Added MongoDB.Driver package reference and project reference to `DaprAspire.Consolidation.Domain`.
    - Introduced `IProjectionRepository.cs` interface for generic projection repository with methods for retrieving and upserting projections.
    - Implemented `MongoProjectionRepository.cs` class, providing MongoDB-based functionality for the `IProjectionRepository<T>` interface, including methods for fetching and upserting projections.

---
+++ DaprAspire.Consolidation.Infrastructure/Repositories/IProjectionRepository.cs
@@ -0,0 +1,31 @@
+﻿using DaprAspire.Consolidation.Domain.Projections;
+
+namespace DaprAspire.Consolidation.Infrastructure.Repositories
+{
+    public interface IProjectionRepository<T> where T : Projection
+    {
+        /// <summary>
+        /// Get a projection by its Id.
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
+        public Task<T?> GetByIdAsync(string id, CancellationToken cancellationToken = default);
+
+        /// <summary>
+        /// Get a projection by its LedgerId.
+        /// </summary>
+        /// <param name="ledgerId"></param>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
+        Task<T?> GetByLedgerIdAsync(string ledgerId, CancellationToken cancellationToken = default);
+
+        /// <summary>
+        /// Upsert a projection. If the projection does not exist, it will be created. If it does exist, it will be updated.
+        /// </summary>
+        /// <param name="projection"></param>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
+        public Task UpsertAsync(T projection, CancellationToken cancellationToken = default);
+    }
+}
---
+++ DaprAspire.Consolidation.Infrastructure/Repositories/MongoProjectionRepository.cs
@@ -0,0 +1,38 @@
+﻿using DaprAspire.Consolidation.Domain.Projections;
+
+using MongoDB.Driver;
+
+namespace DaprAspire.Consolidation.Infrastructure.Repositories
+{
+    public class MongoProjectionRepository<T>(IMongoDatabase database) : IProjectionRepository<T> where T : Projection
+    {
+        private static readonly string _collectionName = typeof(T).Name.ToLowerInvariant() + "s";
+        private readonly IMongoCollection<T> _collection = database.GetCollection<T>(_collectionName);
+
+        public async Task<T?> GetByIdAsync(string id, CancellationToken cancellationToken = default)
+        {
+            return await _collection
+                .Find(p => p.Id == id)
+                .FirstOrDefaultAsync(cancellationToken);
+        }
+
+        public async Task<T?> GetByLedgerIdAsync(string ledgerId, CancellationToken cancellationToken = default)
+        {
+            return await _collection
+                .Find(p => p.LedgerId == ledgerId)
+                .FirstOrDefaultAsync(cancellationToken);
+        }
+
+        public async Task UpsertAsync(T projection, CancellationToken cancellationToken = default)
+        {
+            projection.LastUpdated = DateTimeOffset.UtcNow.UtcDateTime;
+
+            await _collection.ReplaceOneAsync(
+                filter: p => p.Id == projection.Id,
+                replacement: projection,
+                options: new ReplaceOptions { IsUpsert = true },
+                cancellationToken: cancellationToken
+            );
+        }
+    }
+}
