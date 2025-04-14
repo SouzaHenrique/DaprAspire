79f6264e6a7e8763a873c7c401148f73bc02ea01
Henrique Souza <hszm2094@outlook.com>
2025-04-14 12:19:16 -0300

    Remove MongoDB attributes and user property from tokens
    Eliminate `using MongoDB.Bson.Serialization.Attributes;` from
    `ApplicationUserClaim.cs`, `ApplicationUserRole.cs`, and
    `ApplicationUserToken.cs`. Additionally, remove the `User`
    property from `ApplicationUserToken.cs`, indicating a
    refactor of the relationship between tokens and users.

--- DaprAspire.IdentityService.Domain/Entities/Identity/ApplicationUserClaim.cs
+++ DaprAspire.IdentityService.Domain/Entities/Identity/ApplicationUserClaim.cs
@@ -1,7 +1,5 @@
 ﻿using Microsoft.AspNetCore.Identity;
 
-using MongoDB.Bson.Serialization.Attributes;
-
 namespace DaprAspire.IdentityService.Domain.Entities.Identity
 {
     public class ApplicationUserClaim : IdentityUserClaim<string>
