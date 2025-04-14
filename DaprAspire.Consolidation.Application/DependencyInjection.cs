3d6009ebaeb2ae29735f7805a303cb9132088ecb
Henrique Souza <hszm2094@outlook.com>
2025-04-13 21:47:57 -0300

    Add projection service and update project references
    Updated `DaprAspire.Consolidation.Application.csproj` to include new package and project references. Introduced `DependencyInjection` class for service registration, and added `EntryProjectionService` implementing `IEntryProjectionService` for handling projection messages. Created interface to define service methods for message application and balance retrieval.

---
+++ DaprAspire.Consolidation.Application/DependencyInjection.cs
@@ -0,0 +1,52 @@
+﻿using DaprAspire.Consolidation.Application.Services.ProjectionService;
+using DaprAspire.Consolidation.Infrastructure.Repositories;
+
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+using MongoDB.Driver;
+
+namespace DaprAspire.Consolidation.Application
+{
+    public static class DependencyInjection
+    {
+        /// <summary>
+        /// Add the repositories to the service collection.
+        /// </summary>
+        /// <param name="services"></param>
+        /// <returns></returns>
+        public static IServiceCollection AddRepositories(this IServiceCollection services)
+        {
+            services.AddSingleton(typeof(IProjectionRepository<>), typeof(MongoProjectionRepository<>));
+            return services;
+        }
+
+        /// <summary>
+        /// Add the MongoDB client to the service collection.
+        /// </summary>
+        /// <param name="services"></param>
+        /// <param name="config"></param>
+        /// <returns></returns>
+        public static IServiceCollection AddMongo(this IServiceCollection services, IConfiguration config)
+        {
+            services.AddSingleton(sp =>
+            {
+                var mongoClient = new MongoClient(config.GetConnectionString("mongodb"));
+                return mongoClient.GetDatabase("consolidation");
+            });
+
+            return services;
+        }
+
+        /// <summary>
+        /// Add the projection services to the service collection.
+        /// </summary>
+        /// <param name="services"></param>
+        /// <returns></returns>
+        public static IServiceCollection AddProjectionServices(this IServiceCollection services)
+        {
+            services.AddScoped<IEntryProjectionService, EntryProjectionService>();
+            return services;
+        }
+    }
+}
---
+++ DaprAspire.Consolidation.Application/Services/ProjectionService/IEntryProjectionService.cs
@@ -0,0 +1,40 @@
+﻿using DaprAspire.Consolidation.Domain.Projections;
+using DaprAspire.Domain.CrossCutting.Messaging.Events;
+
+namespace DaprAspire.Consolidation.Application.Services.ProjectionService
+{
+    /// <summary>
+    /// Interface for the entry projection service.
+    /// </summary>
+    public interface IEntryProjectionService
+    {
+        /// <summary>
+        /// Apply the entry created message to the projection.
+        /// </summary>
+        /// <param name="message"></param>
+        /// <returns></returns>
+        public Task ApplyAsync(EntryCreatedMessage message);
+
+        /// <summary>
+        /// Apply the entry credited message to the projection.
+        /// </summary>
+        /// <param name="message"></param>
+        /// <returns></returns>
+        public Task ApplyAsync(EntryCreditedMessage message);
+
+        /// <summary>
+        /// Apply the entry debited message to the projection.
+        /// </summary>
+        /// <param name="message"></param>
+        /// <returns></returns>
+        public Task ApplyAsync(EntryDebitedMessage message);
+
+        /// <summary>
+        /// Get the latest daily balance projection for a given ledger ID.
+        /// </summary>
+        /// <param name="ledgerId"></param>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
+        public Task<DailyBalanceProjection?> GetLatestDailyBalanceAsync(string ledgerId, CancellationToken cancellationToken = default);
+    }
+}
