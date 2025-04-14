6aac02aa7ecf0c4df995054c09b4b7053788c63b
Henrique Souza <hszm2094@outlook.com>
2025-04-14 10:58:20 -0300

    Update project to .NET 9.0 and add identity services
    - Updated `DaprAspire.IdentityService.Application.csproj` to target .NET 9.0, enabling implicit usings and nullable reference types. Added project references to the domain and infrastructure projects.
    - Modified `DependencyInjectionExtensions.cs` to include new using directives and added a static class with methods for configuring ASP.NET Core Identity and MongoDB services.

---
+++ DaprAspire.IdentityService.Application/DependencyInjectionExtensions.cs
@@ -0,0 +1,37 @@
+﻿using DaprAspire.IdentityService.Domain.Entities.Identity;
+
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using DaprAspire.IdentityService.Infrastructure.Identity.Stores;
+using MongoDB.Driver;
+
+namespace DaprAspire.IdentityService.Application
+{
+    public static class DependencyInjectionExtensions
+    {
+        public static IServiceCollection AddIdentityService(this IServiceCollection services)
+        {
+            services.AddIdentity<ApplicationUser, ApplicationRole>()
+                    .AddUserStore<UserStore<ApplicationUser, ApplicationRole, string>>()
+                    .AddRoleStore<RoleStore<ApplicationRole, string>>()
+                    .AddUserManager<UserManager<ApplicationUser>>()
+                    .AddRoleManager<RoleManager<ApplicationRole>>()
+                    .AddSignInManager<SignInManager<ApplicationUser>>()
+                    .AddDefaultTokenProviders();
+
+            return services;
+        }
+
+        public static IServiceCollection AddMongo(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddSingleton<IMongoClient>(_ =>
+            new MongoClient(configuration.GetConnectionString("MongoDb")));
+
+            services.AddScoped(serviceProvider =>
+                serviceProvider.GetRequiredService<IMongoClient>().GetDatabase("identitydb"));
+
+            return services;
+        }
+    }
+}
