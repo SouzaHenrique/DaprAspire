1b6d5240337c7425636c93e80f575418b418c9b1
Henrique Souza <hszm2094@outlook.com>
2025-04-14 12:19:40 -0300

    Add ApplicationRoleManager and ApplicationUserManager
    Introduce `ApplicationRoleManager` and `ApplicationUserManager` classes to manage roles and users in the identity service. These classes inherit from `RoleManager<ApplicationRole>` and `UserManager<ApplicationUser>`, respectively, and include constructors for enhanced customization. Additional using directives for identity management and logging have been added.

---
+++ DaprAspire.IdentityService.Application/Commom/Managers/ApplicationUserManager.cs
@@ -0,0 +1,20 @@
+﻿using DaprAspire.IdentityService.Domain.Entities.Identity;
+
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace DaprAspire.IdentityService.Application.Commom.Managers
+{
+    public class ApplicationUserManager(IUserStore<ApplicationUser> store,
+                                        IOptions<IdentityOptions> optionsAccessor,
+                                        IPasswordHasher<ApplicationUser> passwordHasher,
+                                        IEnumerable<IUserValidator<ApplicationUser>> userValidators,
+                                        IEnumerable<IPasswordValidator<ApplicationUser>> passwordValidators,
+                                        ILookupNormalizer keyNormalizer, IdentityErrorDescriber errors,
+                                        IServiceProvider services,
+                                        ILogger<UserManager<ApplicationUser>> logger)
+        : UserManager<ApplicationUser>(store, optionsAccessor, passwordHasher, userValidators, passwordValidators, keyNormalizer, errors, services, logger)
+    {
+    }
+}
