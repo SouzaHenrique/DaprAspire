61919b9d03ce526cb3cca6f7f4c494d797fcffc8
Henrique Souza <hszm2094@outlook.com>
2025-04-14 10:58:06 -0300

    Update identity service to .NET 9.0 with MongoDB support
    - Updated `DaprAspire.IdentityService.Infrastructure.csproj` to target .NET 9.0 and added necessary package references.
    - Introduced `MongoUtil.cs` for MongoDB utility methods, including async item retrieval.
    - Added `RoleStore.cs` for role management with MongoDB, supporting role creation, updates, deletions, and concurrency handling.
    - Added `UserStore.cs` for user management with MongoDB, featuring user creation, deletion, role and token management, and support for two-factor authentication and email confirmation.

---
+++ DaprAspire.IdentityService.Infrastructure/Identity/Stores/MongoUtil.cs
@@ -0,0 +1,28 @@
+﻿using System.Linq.Expressions;
+
+using MongoDB.Driver;
+
+namespace DaprAspire.IdentityService.Infrastructure.Identity.Stores
+{
+    public static class MongoUtil
+    {
+        private static FindOptions<TItem> LimitOneOption<TItem>() => new FindOptions<TItem>
+        {
+            Limit = 1
+        };
+
+        public static async Task<TItem> FirstOrDefaultAsync<TItem>(this IMongoCollection<TItem> collection, Expression<Func<TItem, bool>> p, CancellationToken cancellationToken = default)
+        {
+            if (collection == null) throw new ArgumentNullException(nameof(collection));
+
+            return await (await collection.FindAsync(p, LimitOneOption<TItem>(), cancellationToken).ConfigureAwait(false)).FirstOrDefaultAsync().ConfigureAwait(false);
+        }
+
+        public static async Task<IEnumerable<TItem>> WhereAsync<TItem>(this IMongoCollection<TItem> collection, Expression<Func<TItem, bool>> p, CancellationToken cancellationToken = default)
+        {
+            if (collection == null) throw new ArgumentNullException(nameof(collection));
+
+            return (await collection.FindAsync(p, cancellationToken: cancellationToken).ConfigureAwait(false)).ToEnumerable();
+        }
+    }
+}
