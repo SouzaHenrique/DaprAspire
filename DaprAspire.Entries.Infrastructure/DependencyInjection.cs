a8c60d8c8e20ed71c200c776aa137729d446159d
Henrique Souza <hszm2094@outlook.com>
2025-04-15 00:50:38 -0300

    Add Infrastructure project and add MongoDB support
    - Updated `DaprAspire.Entries.Infrastructure.csproj` to target .NET 9.0 and added necessary package references for configuration and MongoDB.
    - Introduced `DependencyInjection` class in `DependencyInjection.cs` with methods to add MongoDB client and repository services.
    - Created `LedgerEntryReadModelRepository.cs` to implement `ILedgerEntryReadModelRepository`, providing methods for paginated data retrieval and document counting from MongoDB.

---
+++ DaprAspire.Entries.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,41 @@
+﻿using DaprAspire.Entries.Domain.ServiceDefinitions.Persistence;
+using DaprAspire.Entries.Infrastructure.Persistence;
+
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+using MongoDB.Driver;
+
+namespace DaprAspire.Entries.Infrastructure
+{
+    public static class DependencyInjection
+    {
+        /// <summary>
+        /// Add the MongoDB client to the service collection.
+        /// </summary>
+        /// <param name="services"></param>
+        /// <param name="config"></param>
+        /// <returns></returns>
+        public static IServiceCollection AddMongo(this IServiceCollection services, IConfiguration config)
+        {
+            services.AddSingleton(sp =>
+            {
+                var mongoClient = new MongoClient(config.GetConnectionString("mongodb"));
+                return mongoClient.GetDatabase("entries");
+            });
+
+            return services;
+        }
+
+        /// <summary>
+        /// Add the repositories to the service collection.
+        /// </summary>
+        /// <param name="services"></param>
+        /// <returns></returns>
+        public static IServiceCollection AddRepositories(this IServiceCollection services)
+        {
+            services.AddScoped<ILedgerEntryReadModelRepository, LedgerEntryReadModelRepository>();
+            return services;
+        }
+    }
+}
---
+++ DaprAspire.Entries.Infrastructure/Persistence/LedgerEntryReadModelRepository.cs
@@ -0,0 +1,35 @@
+﻿using DaprAspire.Entries.Domain.ReadModels;
+using DaprAspire.Entries.Domain.ServiceDefinitions.Persistence;
+
+using MongoDB.Driver;
+
+namespace DaprAspire.Entries.Infrastructure.Persistence
+{
+    public class LedgerEntryReadModelRepository : ILedgerEntryReadModelRepository
+    {
+        private readonly IMongoCollection<LedgerEntryReadModel> _collection;
+
+        public LedgerEntryReadModelRepository(IMongoDatabase database)
+        {
+            _collection = database.GetCollection<LedgerEntryReadModel>("eventflow-ledgerentryreadmodel");
+        }
+
+        public async Task<IReadOnlyList<LedgerEntryReadModel>> GetPagedAsync(int pageNumber, int pageSize, CancellationToken cancellationToken = default)
+        {
+            var skip = (pageNumber - 1) * pageSize;
+
+            var result = await _collection
+                .Find(FilterDefinition<LedgerEntryReadModel>.Empty)
+                .Skip(skip)
+                .Limit(pageSize)
+                .ToListAsync(cancellationToken);
+
+            return result;
+        }
+
+        public async Task<long> CountAsync(CancellationToken cancellationToken = default)
+        {
+            return await _collection.CountDocumentsAsync(FilterDefinition<LedgerEntryReadModel>.Empty, cancellationToken: cancellationToken);
+        }
+    }
+}
