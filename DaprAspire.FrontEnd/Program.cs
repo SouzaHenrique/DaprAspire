09429bbab7bedec9d878c38754149bebb4ed1318
Henrique Souza <hszm2094@outlook.com>
2025-04-14 22:51:26 -0300

    Add MudBlazor integration and update project structure
    - Updated `DaprAspire.FrontEnd.csproj` to include MudBlazor package and added new folders for Components, Models, and Services.
    - Enhanced `MainLayout.razor` with MudBlazor components for a modern UI layout.
    - Modified `Program.cs` to register MudBlazor services.
    - Disabled automatic browser launch in `launchSettings.json`.
    - Updated `_Imports.razor` to include MudBlazor namespace for easier component usage.

--- DaprAspire.FrontEnd/Program.cs
+++ DaprAspire.FrontEnd/Program.cs
@@ -1,5 +1,7 @@
-using Microsoft.AspNetCore.Components.Web;
+﻿using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+using MudBlazor.Services;
 
 namespace DaprAspire.FrontEnd
 {
@@ -12,6 +14,7 @@ namespace DaprAspire.FrontEnd
             builder.RootComponents.Add<HeadOutlet>("head::after");
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.Services.AddMudServices();
 
             await builder.Build().RunAsync();
         }
